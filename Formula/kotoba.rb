f5e0e8a1f621ff40e44b083656315017c6a010b3
Jun Kawasaki <jun784@gmail.com>
2026-07-17 20:04:31 +0900

    Install JVM-free native CLI with Homebrew

--- Formula/kotoba.rb
+++ Formula/kotoba.rb
@@ -1,26 +1,36 @@
 class Kotoba < Formula
-  desc "CLJC/EDN-authoritative Kotoba CLI launcher"
+  desc "Capability-safe Kotoba language compiler and CLI"
   homepage "https://github.com/kotoba-lang/kotoba"
-  url "https://github.com/kotoba-lang/kotoba/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "cdf1099d970aa6e5be0e88099fc58bcefea58cadec63392863c3b7538a883655"
+  url "https://github.com/kotoba-lang/kotoba/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "bcb06b31fb01d015ccc7fc0c3085fdce26735fcfb7f30aa90f5c757bdd6a8138"
   license "Apache-2.0"
-  head "https://github.com/kotoba-lang/kotoba.git", branch: "main"
 
-  depends_on "clojure"
+  resource "binary" do
+    on_macos do
+      on_arm do
+        url "https://github.com/kotoba-lang/kotoba/releases/download/v0.6.1/kotoba-darwin-arm64.tar.gz"
+        sha256 "50c17d701f7a157ae065f5c104774acc80c0f229df45ed842da97ba7aeb4578b"
+      end
+      on_intel do
+        url "https://github.com/kotoba-lang/kotoba/releases/download/v0.6.1/kotoba-darwin-amd64.tar.gz"
+        sha256 "93491c872329be5607aae0a446a0bac30c7f6e58cb43e20ee60cd154bcc3651f"
+      end
+    end
+    on_linux do
+      url "https://github.com/kotoba-lang/kotoba/releases/download/v0.6.1/kotoba-linux-amd64.tar.gz"
+      sha256 "08885fd9a6ba847b25cc6b85753298f6ba829fb2b7e016c9f137fc415ffa482d"
+    end
+  end
 
   def install
-    libexec.install "deps.edn", "src", "bin"
-    (bin/"kotoba").write <<~EOS
-      #!/bin/sh
-      export KOTOBA_CLJ_HOME="#{libexec}"
-      exec "#{libexec}/bin/kotoba-clj" "$@"
-    EOS
+    resource("binary").stage do
+      bin.install "kotoba"
+    end
   end
 
   test do
-    ENV["KOTOBA_CLJ_HOME"] = libexec
-    out = shell_output("#{bin}/kotoba check --kind cli-contract --json")
-    assert_match "kotoba.cli", out
-    assert_match "command-count", out
+    output = shell_output("#{bin}/kotoba selfhost check --json")
+    assert_match '"kotoba.cli\\/ok?":true', output
+    assert_match '"kotoba.cli\\/code":"valid"', output
   end
 end
